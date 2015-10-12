81be23b094d73aa70a279bc2ed063df2c7a42b13
Ken OKADA <keno.ss57@gmail.com>
2015-10-13 01:55:25 +0900

    Clean up

--- common/zsh/.zsh.d/config/tmp.sh
+++ common/zsh/.zsh.d/config/tmp.sh
@@ -100,8 +100,6 @@ export LSCOLORS=gxdxxxxxcxxAxAcAcAgAgA
 #export LS_COLORS='di=01;34:ln=01;35:so=01;32:ex=01;31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 zstyle ':completion:*' list-colors 'di=;36' 'ln=;33' 'ex=32'
 
-function lsEU() { ls $@ | iconv -f EUC-JP -t UTF-8 }
-
 
 
 

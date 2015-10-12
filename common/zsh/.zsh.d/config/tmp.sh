010236b820e64a668fbc9b5b8286e3e5961e7bb8
Ken OKADA <keno.ss57@gmail.com>
2015-10-12 20:17:38 +0900

    Add JSON-RPC utility

--- common/zsh/.zsh.d/config/tmp.sh
+++ common/zsh/.zsh.d/config/tmp.sh
@@ -707,3 +707,24 @@ function p () {
 function peco-ssh () {
     ssh $(grep -E '^Host[[:space:]]+[^*]' ~/.ssh/config | peco | awk '{print $2}')
 }
+
+
+
+function normalize-json () {
+    local str="$1"
+
+    node -e 'var str = process.argv[1]; var obj = (new Function("return " + str))(); console.log(JSON.stringify(obj));' $str
+}
+
+function curl-jsonrpc-request () {
+    local url="$1"
+    local method="\"$2\""
+    local params="$3"
+    local raw_json="{ jsonrpc: '2.0', 'id': 1, 'method': $method, 'params': $params }"
+    local json="$(normalize-json $raw_json)"
+
+    curl -s $url \
+         -H "Accept: application/json" -H 'Content-Type: application/json' \
+         -d $json \
+         ${@:4} \
+}

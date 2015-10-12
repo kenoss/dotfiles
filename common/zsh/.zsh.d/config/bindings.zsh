df9b23f1eee29bf8f69908f66b87820decb42340
Ken OKADA <keno.ss57@gmail.com>
2015-10-13 03:06:37 +0900

    Factor out peco git feature

--- common/zsh/.zsh.d/config/bindings.zsh
+++ common/zsh/.zsh.d/config/bindings.zsh
@@ -9,3 +9,11 @@ bindkey "^y" paste-as-yank
 # Expand abbrev
 load-zsh-config config/feature-expand-abbrev.zsh
 bindkey "^e" expand-abbrev
+
+
+# peco git
+load-zsh-config config/feature-peco-git.zsh
+bindkey "^u^a" peco-select-gitadd
+bindkey '^u^b' peco-git-branch-checkout
+bindkey '^u^n' peco-git-new-branch-with-base
+bindkey '^u^u' peco-git-select-branch

e1ec9942c2de9459abf7699fad6ecea9b87db1c7
Ken OKADA <keno.ss57@gmail.com>
2015-10-26 01:25:16 +0900

    Factor out miscellaneous setting to misc.zsh

--- common/zsh/.zsh.d/config/misc.zsh
+++ common/zsh/.zsh.d/config/misc.zsh
@@ -1,5 +1,119 @@
 # -*- mode: shell-script -*-
 
+HOSTNAME=$(hostname)
+
+## LANG
+if [ "$TERM" = "cons25" ]; then
+  export LANG=C
+#  export LC_ALL=C
+else
+#  export LANG=ja_JP.eucJP
+#  export LC_ALL=ja_JP.eucJP
+#  export LANG=ja_JP.UTF-8
+#  export LC_ALL=C
+  export LANG="ja_JP.UTF-8"
+  export LC_ALL="C"
+  # export LC_ALL="ja_JP.UTF-8"
+  # export LC_CTYPE="ja_JP.UTF-8"
+  # export LC_NUMERIC="ja_JP.UTF-8"
+  # export LC_TIME="ja_JP.UTF-8"
+  # export LC_COLLATE="ja_JP.UTF-8"
+  # export LC_MONETARY="ja_JP.UTF-8"
+  # export LC_MESSAGES="ja_JP.UTF-8"
+  # export LC_PAPER="ja_JP.UTF-8"
+  # export LC_NAME="ja_JP.UTF-8"
+  # export LC_ADDRESS="ja_JP.UTF-8"
+  # export LC_TELEPHONE="ja_JP.UTF-8"
+  # export LC_MEASUREMENT="ja_JP.UTF-8"
+  # export LC_IDENTIFICATION="ja_JP.UTF-8"
+fi
+
+export LC_ALL=ja_JP.UTF-8
+
+
+eval `tset -s rxvt-256color` > /dev/null
+
+
+
+#
+###
+### Zsh options
+###
+
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt correct
+setopt nolistbeep
+setopt complete_aliases
+setopt noautomenu
+setopt extended_glob
+setopt brace_ccl
+
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+
+setopt interactive_comments
+setopt noautoremoveslash
+
+
+## --prefix=~/localというように「=」の後でも
+## 「~」や「=コマンド」などのファイル名展開を行う。
+setopt magic_equal_subst
+
+## 実行したプロセスの消費時間が3秒以上かかったら
+## 自動的に消費時間の統計情報を表示する。
+REPORTTIME=3
+
+## 「/」も単語区切りとみなす。
+WORDCHARS=${WORDCHARS:s,/,,}
+
+
+#
+###
+### Completion
+###
+
+zstyle :compinstall filename "$HOME/.zshrc"
+
+fpath=(~/.zsh.d/completion $fpath)
+autoload -Uz compinit
+compinit
+
+
+
+#
+###
+### History
+###
+
+HISTFILE=$ZDOTDIR/.zsh_history
+HISTSIZE=1000000
+SAVEHIST=1000000
+setopt hist_ignore_dups
+setopt share_history
+setopt hist_verify
+setopt hist_ignore_space
+setopt hist_reduce_blanks
+
+zshaddhistory() {
+    local line=${1%%$'\n'}
+    local cmd=${line%% *}
+
+    # 以下の条件をすべて満たすものだけをヒストリに追加する
+    [[ ${#line} -ge 5
+        && ${cmd} != l
+        && ${cmd} != c
+        && ${cmd} != m
+        && ${line} != cd*\;xdvi\ -sourceposition*\& # for xdvi-jump-to-line in emacs
+    ]]
+}
+
+
+
+#function cd() { builtin cd $@ && ls }
+function chpwd() { ls }
+
+
+#
 ###
 ### z.sh
 ###
@@ -11,3 +125,23 @@ function precmd () {
 }
 
 
+#
+###
+### prompt
+###
+
+setopt prompt_subst
+export PROMPT='%F{red}%D{%H:%M:%S} ${USER}@${HOSTNAME} %! %(!.#.$)%k%f '
+export PROMPT2='%F{red}         ${USER}@${HOSTNAME} %! >%k%f '
+#export RPROMPT='%F{red}[%f`rprompt-git-current-branch`%~%F{red}]%f'
+
+
+#
+###
+### ls
+###
+
+export LSCOLORS=gxdxcxdxfxegedabagacad
+export LSCOLORS=gxdxxxxxcxxAxAcAcAgAgA
+#export LS_COLORS='di=01;34:ln=01;35:so=01;32:ex=01;31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+zstyle ':completion:*' list-colors 'di=;36' 'ln=;33' 'ex=32'
--- common/zsh/.zsh.d/config/tmp.sh
+++ common/zsh/.zsh.d/config/tmp.sh
@@ -3,102 +3,20 @@
 source $ZDOTDIR/256colorlib.zsh
 # [ -f $ZDOTDIR/.zshrc.$(uname) ] && source $ZDOTDIR/.zshrc.$(uname)
 
-HOSTNAME=$(hostname)
-
-## LANG
-if [ "$TERM" = "cons25" ]; then
-  export LANG=C
-#  export LC_ALL=C
-else
-#  export LANG=ja_JP.eucJP
-#  export LC_ALL=ja_JP.eucJP
-#  export LANG=ja_JP.UTF-8
-#  export LC_ALL=C
-  export LANG="ja_JP.UTF-8"
-  export LC_ALL="C"
-  # export LC_ALL="ja_JP.UTF-8"
-  # export LC_CTYPE="ja_JP.UTF-8"
-  # export LC_NUMERIC="ja_JP.UTF-8"
-  # export LC_TIME="ja_JP.UTF-8"
-  # export LC_COLLATE="ja_JP.UTF-8"
-  # export LC_MONETARY="ja_JP.UTF-8"
-  # export LC_MESSAGES="ja_JP.UTF-8"
-  # export LC_PAPER="ja_JP.UTF-8"
-  # export LC_NAME="ja_JP.UTF-8"
-  # export LC_ADDRESS="ja_JP.UTF-8"
-  # export LC_TELEPHONE="ja_JP.UTF-8"
-  # export LC_MEASUREMENT="ja_JP.UTF-8"
-  # export LC_IDENTIFICATION="ja_JP.UTF-8"
-fi
-
 bindkey -e
-setopt auto_pushd
-setopt pushd_ignore_dups
-setopt correct
-setopt nolistbeep
-setopt complete_aliases
-setopt noautomenu
-setopt extended_glob
-
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-
-setopt interactive_comments
-setopt noautoremoveslash
-
-
-
-eval `tset -s rxvt-256color` > /dev/null
 
 
 
 
 
-# history
-HISTFILE=$ZDOTDIR/.zsh_history
-HISTSIZE=1000000
-SAVEHIST=1000000
-setopt hist_ignore_dups
-setopt share_history
-setopt hist_verify
-setopt hist_ignore_space
-setopt hist_reduce_blanks
-
-zshaddhistory() {
-    local line=${1%%$'\n'}
-    local cmd=${line%% *}
-
-    # 以下の条件をすべて満たすものだけをヒストリに追加する
-    [[ ${#line} -ge 5
-        && ${cmd} != l
-        && ${cmd} != c
-        && ${cmd} != m
-        && ${line} != cd*\;xdvi\ -sourceposition*\& # for xdvi-jump-to-line in emacs
-    ]]
-}
 
 
 
-# compinstall
-zstyle :compinstall filename "$HOME/.zshrc"
-
-autoload -Uz compinit
-compinit
-
-
-### prompt
-setopt prompt_subst
-export PROMPT='%F{red}%D{%H:%M:%S} ${USER}@${HOSTNAME} %! %(!.#.$)%k%f '
-export PROMPT2='%F{red}         ${USER}@${HOSTNAME} %! >%k%f '
-#export RPROMPT='%F{red}[%f`rprompt-git-current-branch`%~%F{red}]%f'
 
 
 
-### ls
-#unset LANG
-export LSCOLORS=gxdxcxdxfxegedabagacad
-export LSCOLORS=gxdxxxxxcxxAxAcAcAgAgA
-#export LS_COLORS='di=01;34:ln=01;35:so=01;32:ex=01;31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
-zstyle ':completion:*' list-colors 'di=;36' 'ln=;33' 'ex=32'
+
+
 
 
 
@@ -117,11 +35,6 @@ bindkey '^S' history-incremental-pattern-search-forward
 
 
 
-#function cd() { builtin cd $@ && ls }
-function chpwd() { ls }
-
-
-
 function cdup() {
     if [ -z "$BUFFER" ]; then
         echo
@@ -133,32 +46,6 @@ function cdup() {
 }
 zle -N cdup
 bindkey '\^' cdup
-
-
-
-
-
-
-
-
-## --prefix=~/localというように「=」の後でも
-## 「~」や「=コマンド」などのファイル名展開を行う。
-setopt magic_equal_subst
-
-## 実行したプロセスの消費時間が3秒以上かかったら
-## 自動的に消費時間の統計情報を表示する。
-REPORTTIME=3
-
-## 「/」も単語区切りとみなす。
-WORDCHARS=${WORDCHARS:s,/,,}
-
-
-
-
-
-
-
-
 
 
 
@@ -188,7 +75,6 @@ function upload-ical () {
 
 
 
-setopt brace_ccl
 
 
 PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
@@ -236,5 +122,3 @@ PERL_MM_OPT="INSTALL_BASE=/Users/takeshi.okada/perl5"; export PERL_MM_OPT;
 
 
 PATH=$PATH:"$HOME/bin.local/activator"
-
-export LC_ALL=ja_JP.UTF-8

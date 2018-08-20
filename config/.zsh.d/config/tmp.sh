a76ed67f3278848b66d8b7a61268d8f926c138a3
Ken OKADA <keno.ss57@gmail.com>
2018-08-20 11:07:52 +0900

    refactor: Zsh: Simplify checking powerline

--- config/.zsh.d/config/tmp.sh
+++ config/.zsh.d/config/tmp.sh
@@ -5,7 +5,7 @@
 ### Powerline
 ###
 
-if (type -p pip && pip show powerline-status) > /dev/null; then
+if type -p powerline > /dev/null; then
     source $(pip show powerline-status | pcregrep -o '(?<=^Location: ).*$')/powerline/bindings/zsh/powerline.zsh
     PS1=$(printf '%s\n%s' "$PS1" '$ ')
 fi

c58a31f9e79d6df75d598fca1a7d84666787fa89
Ken OKADA <keno.ss57@gmail.com>
2019-03-19 10:52:31 +0900

    zsh: Delete unused environment variables

--- config/.zsh.d/config/env.sh
+++ config/.zsh.d/config/env.sh
@@ -1,8 +1,1 @@
 export DOCKER_BUILDKIT=1
-
-###
-### TeX
-###
-export BSTINPUT=$HOME/texmf/bibtex/bst
-export BIBINPUT=$HOME/texmf/bibtex/bib
-alias eplatex="/usr/local/texlive/2012/bin/amd64-freebsd/platex"

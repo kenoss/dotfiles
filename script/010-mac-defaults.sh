7a7008b4d2f9be73544f69aa9615624988ed43fc
Ken OKADA <keno.ss57@gmail.com>
2019-09-20 13:32:04 +0900

    mac: dock

--- script/010-mac-defaults.sh
+++ script/010-mac-defaults.sh
@@ -1,2 +1,16 @@
+###
+### Keyboard
+###
+
 defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 1
+
+
+###
+### Dock
+###
+
+# Clear default persistent apps.
+defaults write com.apple.dock persistent-apps -array
+# Small icon size.
+defaults write com.apple.dock tilesize -int 25

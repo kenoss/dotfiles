412af5c07ef7c47a40c32ff6f6acfd4f56f7f741
Ken OKADA <keno.ss57@gmail.com>
2019-10-10 04:02:51 +0900

    font-ricty: Support linux

--- cookbooks/font-ricty/default.rb
+++ cookbooks/font-ricty/default.rb
@@ -1,7 +1,52 @@
 case node[:platform]
 when 'darwin'
-  execute "cp -a ./cookbooks/font-ricty/files/repo/powerline-fontpatched/*.ttf #{ENV['HOME']}/Library/Fonts" do
-    not_if "test -f '#{ENV['HOME']}/Library/Fonts/Ricty Diminished Discord Regular for Powerline.ttf'"
+  fonts_dir = "#{ENV['HOME']}/Library/Fonts"
+when 'ubuntu'
+  fonts_dir = "#{ENV['HOME']}/.fonts"
+else
+  raise 'not supported platform'
+end
+
+directory fonts_dir do
+  user node[:user]
+end
+
+
+define :ricty_font, file: nil do
+  repo = 'https://github.com/mzyy94/RictyDiminished-for-Powerline'
+  file = "#{params[:name]}"
+  url = "#{repo}/raw/master/powerline-fontpatched/#{file.gsub(/ /, '%20')}"
+
+  dest = "#{fonts_dir}/#{file}"
+
+  execute "curl -fSL #{url} -o '#{dest}'" do
+    not_if "test -f '#{dest}'"
+    user node[:user]
+  end
+end
+
+
+ls_output_before = run_command("ls -1 #{fonts_dir}/Ricty* | wc -l", error: false).stdout
+
+ricty_font 'Ricty Diminished Bold Oblique for Powerline.ttf'
+ricty_font 'Ricty Diminished Bold for Powerline.ttf'
+ricty_font 'Ricty Diminished Bold Oblique for Powerline.ttf'
+ricty_font 'Ricty Diminished Discord Bold for Powerline.ttf'
+ricty_font 'Ricty Diminished Discord Bold Oblique for Powerline.ttf'
+ricty_font 'Ricty Diminished Discord Oblique for Powerline.ttf'
+ricty_font 'Ricty Diminished Discord Regular for Powerline.ttf'
+ricty_font 'Ricty Diminished Oblique for Powerline.ttf'
+ricty_font 'Ricty Diminished Regular for Powerline.ttf'
+
+ls_output_after =  run_command("ls -1 #{fonts_dir}/Ricty* | wc -l", error: false).stdout
+
+
+case node[:platform]
+when 'darwin'
+when 'ubuntu'
+  execute 'fc-cache -fv' do
+    not_if ls_output_before == ls_output_after
+    user node[:user]
   end
 else
   raise 'not supported platform'

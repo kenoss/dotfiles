41ef90db784657f4b4ad66ce7cc0158df0aab045
Ken OKADA <keno.ss57@gmail.com>
2019-10-17 03:45:46 +0900

    Linux support: roswell

--- cookbooks/roswell/default.rb
+++ cookbooks/roswell/default.rb
@@ -1,26 +1,42 @@
 lisp = 'sbcl'
 version = '1.5.6'
 default = "#{lisp}/#{version}"
-roswell_home = '$HOME/.roswell'
+roswell_home = "#{ENV['HOME']}/.roswell"
 var = "ROSWELL_HOME=#{roswell_home}"
+ros = "#{ENV['HOME']}/.local/bin/ros"
 
 
-execute 'brew install roswell' do
-  not_if 'type -p ros'
+local_repo = "#{ENV['HOME']}/src/github.com/roswell/roswell"
+
+git local_repo do
+  repository 'https://github.com/roswell/roswell'
+  revision 'origin/release'
+  user node[:user]
 end
 
-execute "#{var} ros setup" do
+execute 'install roswell' do
+  not_if "test -f #{ENV['HOME']}/.local/bin/ros"
+  cwd local_repo
+  user node[:user]
+  command "sh bootstrap && ./configure --prefix=#{ENV['HOME']}/.local && make && make install"
+end
+
+execute "#{var} #{ros} setup" do
   not_if "test -d #{roswell_home}"
+  user node[:user]
 end
 
-execute "#{var} ros install #{default}" do
-  not_if "#{var} ros list installed | grep '#{default}'"
+execute "#{var} #{ros} install #{default}" do
+  not_if "#{var} #{ros} list installed | grep '#{default}'"
+  user node[:user]
 end
 
-execute "#{var} ros use #{default}" do
-  not_if "#{var} [ \"$(printf '%s/%s' $(ros config show default.lisp) $(ros config show #{lisp}.version))\" = '#{default}' ]"
+execute "#{var} #{ros} use #{default}" do
+  not_if "#{var} [ \"$(printf '%s/%s' $(#{ros} config show default.lisp) $(#{ros} config show #{lisp}.version))\" = '#{default}' ]"
+  user node[:user]
 end
 
-execute "#{var} ros install slime" do
+execute "#{var} #{ros} install slime" do
   not_if "test -f #{roswell_home}/helper.el"
+  user node[:user]
 end

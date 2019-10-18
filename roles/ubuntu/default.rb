598f209d5c6409e50d734dd001a5eedd52834cdf
Ken OKADA <keno.ss57@gmail.com>
2019-10-18 20:48:15 +0900

    wip ubuntu

---
+++ roles/ubuntu/default.rb
@@ -0,0 +1,36 @@
+include_role 'base'
+
+include_cookbook 'git'
+
+include_cookbook 'rbenv'
+
+include_cookbook 'pyenv'
+pip 'virtualenv'
+pip 'powerline-status'
+
+include_cookbook 'ndenv'
+
+include_cookbook 'rust'
+# cargo 'racer'
+# cargo 'cargo-edit'
+# cargo 'cargo-script'
+# cargo 'cargo-update'
+
+include_cookbook 'roswell'
+
+include_cookbook 'vim'
+include_cookbook 'zsh'
+include_cookbook 'ghq'
+include_cookbook 'peco'
+
+include_cookbook 'font-ricty'
+
+dotfile '.config/alacritty'
+dotfile '.config/karabiner'
+dotfile '.config/pet'
+dotfile '.config/powerline'
+dotfile '.peco'
+dotfile '.tmux'
+dotfile '.tmux.conf'
+dotfile '.tigrc'
+dotfile '.uim.d'

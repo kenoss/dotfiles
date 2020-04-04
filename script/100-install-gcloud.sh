7f1d59000178ebba3edbc1eab611725eedfa463c
Ken Okada <keno.ss57@gmail.com>
2020-04-05 04:25:01 +0900

    Update gcloud installation script

--- script/100-install-gcloud.sh
+++ script/100-install-gcloud.sh
@@ -3,22 +3,23 @@
 set -e
 
 
-version="196.0.0"
-sha256_linux="4de5cc9b9e70cf08150cc7595a89a69efffee0f1e4c55dcb60c16601bfd5f0fc"
-sha256_darwin="985ed75c67637041182425fe6669df66693de5ad89335d900903a7b8aa22bbe1"
-tar_path="$HOME/bin/google-cloud-sdk.tar.gz"
+version='280.0.0'
+sha256_linux='11950f1db216ec7dc3abaf80722fb80518c38e279bd76b6924326fe660c209cf'
+sha256_darwin='c9554507bc217a503b42bef7dfa72179bae57ad7e4e696af4205c50b373d3576'
+target_dir="$HOME/.local/bin"
+tar_path="$target_dir/google-cloud-sdk.tar.gz"
 
-if [ -d "$HOME/bin/google-cloud-sdk" ]; then
+if [ -d "$target_dir/google-cloud-sdk" ]; then
     echo 'google-cloud-sdk is already installed'
     exit 0
 fi
 
 case "$(uname)" in
-    "Linux")
+    'Linux')
         sha256_target="$sha256_linux"
         os_arch='linux-x86_64'
         ;;
-    "Darwin")
+    'Darwin')
         sha256_target="$sha256_darwin"
         os_arch='darwin-x86_64'
         ;;
@@ -35,8 +36,8 @@ if [ "$sha256" != "$sha256_target" ]; then
     exit 1
 fi
 
-cd ~/bin
+cd "$target_dir"
 tar xvzf "$tar_path"
 rm "$tar_path"
 
-bash ~/bin/google-cloud-sdk/install.sh
+bash "$target_dir/google-cloud-sdk/install.sh"

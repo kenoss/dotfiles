efcf4b9f9119c9d46eb05e0bed918cefa0f0808a
keno <keno.ss57@gmail.com>
2022-12-30 16:26:12 +0900

    refactor: Add role `standard`

--- roles/ubuntu/default.rb
+++ roles/ubuntu/default.rb
@@ -1,35 +1,9 @@
-include_role 'base'
+include_role 'standard'
 
-include_cookbook 'git'
+package 'rofi'
+package 'xmonad'
+package 'xmonad-contrib'
 
 # include_cookbook 'rbenv'
-
-include_cookbook 'pyenv'
-
+# include_cookbook 'pyenv'
 # include_cookbook 'ndenv'
-
-include_cookbook 'rust'
-# cargo 'racer'
-# cargo 'cargo-edit'
-# cargo 'cargo-script'
-# cargo 'cargo-update'
-
-# include_cookbook 'roswell'
-
-include_cookbook 'vim'
-include_cookbook 'zsh'
-include_cookbook 'ghq'
-include_cookbook 'peco'
-include_cookbook 'direnv'
-
-include_cookbook 'font-ricty'
-
-dotfile '.config/alacritty'
-dotfile '.config/karabiner'
-dotfile '.config/pet'
-dotfile '.config/powerline'
-dotfile '.peco'
-dotfile '.tmux'
-dotfile '.tmux.conf'
-dotfile '.tigrc'
-dotfile '.uim.d'

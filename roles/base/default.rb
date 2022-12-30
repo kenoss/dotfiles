77798e2e63d79917762ba35fa0c5567816c49725
keno <keno.ss57@gmail.com>
2022-12-30 16:11:32 +0900

    Add build-essential to base

--- roles/base/default.rb
+++ roles/base/default.rb
@@ -3,8 +3,10 @@ include_cookbook 'functions'
 case node[:platform]
 when 'darwin' then
 when 'ubuntu', 'debian' then
+  package 'build-essential'
   package 'pkg-config'
 when 'arch' then
+  package 'base-devel'
   package 'pkgconf'
 else
   raise "not supported platform: #{node[:platform]}"

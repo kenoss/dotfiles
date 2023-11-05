e0caf867555e1ece72a48a1c11160235e88c0601
keno <keno.ss57@gmail.com>
2023-11-05 17:57:12 +0900

    zsh: `export GPT_TTY`

--- config/.zsh.d/config/env.sh
+++ config/.zsh.d/config/env.sh
@@ -3,3 +3,6 @@ export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
 
 export DOCKER_BUILDKIT=1
+
+# https://gnupg.org/documentation/manuals/gnupg/Common-Problems.html
+export GPG_TTY=$(tty)

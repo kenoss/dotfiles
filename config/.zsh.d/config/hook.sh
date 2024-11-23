2d5c16c6e90ef4f7aa1c78c1631b18befb98d771
kenoss <keno.ss57@gmail.com>
2024-11-23 23:54:11 +0900

    zsh: Prevent preexec prints environment variables

--- config/.zsh.d/config/hook.sh
+++ config/.zsh.d/config/hook.sh
@@ -1,8 +1,12 @@
 if [ -n "$TMUX" ]; then
-  function refresh-tmux-environment-variables {
+  function refresh-tmux-environment-variables-aux {
     export $(tmux show-environment | grep "^DISPLAY")
     export $(tmux show-environment | grep "^SSH_AGENT_PID")
     export $(tmux show-environment | grep "^SSH_AUTH_SOCK")
+  }
+
+  function refresh-tmux-environment-variables {
+    refresh-tmux-environment-variables-aux >/dev/null
   }
 else
   function refresh-tmux-environment-variables {}
